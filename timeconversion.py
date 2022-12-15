ffc828f9be64460a781226565fbc0db4c73e2e1f
Tish Faroul <lfaroul14@gmail.com>
2022-12-15 13:49:31 -0500

    added comments to time conversion

--- timeconversion.py
+++ timeconversion.py
@@ -11,9 +11,15 @@
 # Return '00:01:00'.
 
 def timeConversion(s):
+#grab the last two characters (PM or AM)
     lastTwo = s[-2:]
+# if the last two chars is equal to PM AND the first two doesn't equal 12
     if lastTwo == "PM" and s[:2] != "12":
+# add 12 to the first two chars (after making it into a num), then convert it back to a string
+#  add PM to the end after making it a string.
         s = str(12 + int(s[:2])) + s[2:]
+# if the last two chars is equal to AM AND the first two equals 12
     if lastTwo == "AM" and s[:2] == "12":
+#put 00 at the beginning of the string and add the rest of the string
         s = "00" + s[2:]
     return s[:-2]
